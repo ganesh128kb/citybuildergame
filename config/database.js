da9dcba246d9c144f369be9b63a3acc01abb4731
unknown <ganesh128kb@gmail.com>
2015-11-28 12:39:27 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	Obj : {connectionLimit: 3, host: 'us-cdbr-iron-east-03.cleardb.net', user: 'b4d31af6d550b4', password: '0154f9c5', database: 'heroku_09e37760ad61c7f'}
+	Obj : {connectionLimit: 3, host: 'us-cdbr-iron-east-03.cleardb.net', user: 'b1d2a95bf2929c', password: '6c52b12f', database: 'heroku_31d6088765efa80'}
 }
 
